3c9e106133ae45145609809fd7f6cdf3c87acfaa
pc-home-lenovo <luise.albor@gmail.com>
2026-03-01 18:14:24 -0500

    add pdf for sale

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,17 +1,29 @@
-import { HttpInterceptorFn, HttpHeaders } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   const user = authService.currentUser();
 
+  let modifiedReq = req;
+
   if (user?.token) {
-    const authReq = req.clone({
+    modifiedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${user.token}`)
     });
-    return next(authReq);
   }
 
-  return next(req);
+  return next(modifiedReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
 };
